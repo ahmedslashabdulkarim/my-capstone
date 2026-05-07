85649820dec6a71bdb3bdb3a4b2d4d3287b652e1
ahmed <ahmedslashabdulkarim@gmail.com>
2026-05-07 20:07:11 +0200

    update TodoListPage.tsx for frontend

--- frontend/src/pages/TodoListPage.tsx
+++ frontend/src/pages/TodoListPage.tsx
@@ -1,5 +1,46 @@
 
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+import Loader from "../components/Loader"
+import ErrorMessage from "../components/ErrorMessage"
+import TodoItem from "../components/TodoItem"
+import { fetchTodos, deleteTodo } from "../services/todoApi"
 
+export default function TodoListPage() {
+    const [todos, setTodos] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState("")
+
+    useEffect(() => {
+        fetchTodos()
+            .then(setTodos)
+            .catch(() => setError("فشل تحميل المهام"))
+            .finally(() => setLoading(false))
+    }, [])
+
+    async function handleDelete(id: string) {
+        await deleteTodo(id)
+        setTodos(todos.filter((t: any) => t.id !== id))
+    }
+
+    if (loading) return <Loader />
+    if (error) return <ErrorMessage message={error} />
+
+    return (
+        <div>
+            <h1>Todo List</h1>
+            <Link to="/add">+ Add Todo</Link>
+
+            <ul>
+                {todos.map((todo: any) => (
+                    <TodoItem key={todo.id} todo={todo} onDelete={handleDelete} />
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+/*
 import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 
@@ -33,38 +74,6 @@ export default function TodoListPage() {
                         {todo.description}
                         <br />
                         <Link to={`/edit/${todo.id}`}>Edit</Link>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
-}
-
-
-
-/*
-import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
-
-export default function TodoListPage() {
-    const [todos, setTodos] = useState([])
-
-    useEffect(() => {
-        fetch("http://localhost:8080/api/todos")
-            .then(res => res.json())
-            .then(data => setTodos(data))
-    }, [])
-
-    return (
-        <div>
-            <h1>Todo List</h1>
-            <Link to="/add">Add Todo</Link>
-
-            <ul>
-                {todos.map((todo: any) => (
-                    <li key={todo.id}>
-                        {todo.title}
-                        <Link to={`/edit/${todo.id}`}> Edit</Link>
                     </li>
                 ))}
             </ul>
